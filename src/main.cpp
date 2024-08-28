0cc8052c269810b0e9026343ffe2b6dc0b7c9454
DESKTOP-NU36G62\WANG <wang8763@naver.com>
2024-08-28 23:46:12 +0900

    데이터 송신 시간 수정

--- src/main.cpp
+++ src/main.cpp
@@ -92,7 +92,7 @@ int main(void)
     //figure8Pattern(200, 0, 10, 5);  //moves the ball in an elipse (r, start, wait, num)
     //DEMO(); //does all of the patterns sequentially;
 
-    if(micros() - pre_time >= 100000)
+    if(micros() - pre_time >= 50000)
     {
         pre_time = micros();
         Encode_Msg_Status(&telemetry_tx_buf[0]);
